90cfe9ab23fe8e180748a0cbf43dced7af117299
Matheus Lima <101226374+matheuscaldas1@users.noreply.github.com>
2024-01-18 17:16:16 +0000

    feat(home): adding infos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Hi, my name is Matheus, I have a degree in computer science from Universidade Paulista and I work at Corebiz as a front-end developer";
 
 /**
  * List of some of skills or technologies you work on, are learning,
